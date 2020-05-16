ebdf95069bab70ae065fb5ca0365ff4602f351ef
Jehee Cheon <jeheecheon@gmail.com>
2020-05-16 15:42:05 +0900

    Update

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -2,13 +2,16 @@ var navs = document.getElementsByClassName("subnav");
 
 for (nav of navs) {
     if (nav.classList.contains("subnav--hide") == false) {
-        nav.previousElementSibling.firstElementChild.classList.toggle("fa-caret-square-down");
-        nav.previousElementSibling.firstElementChild.classList.toggle("fa-caret-square-up");
+        nav.previousElementSibling.children[2].classList.toggle("fa-caret-square-down");
+        nav.previousElementSibling.children[2].classList.toggle("fa-caret-square-up");
     }
 }
 
 function toggle_subnav(i) {
     i.parentNode.nextElementSibling.classList.toggle("subnav--hide");
+    if (i.nextElementSibling) {
+        i = i.parentNode.children[2];
+    }
     i.classList.toggle("fa-caret-square-down");
     i.classList.toggle("fa-caret-square-up");
 }
