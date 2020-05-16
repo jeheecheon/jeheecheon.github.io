09e6ac21337639e3ae6b2a37484a22338387f89a
Jehee Cheon <jeheecheon@gmail.com>
2020-05-16 23:43:44 +0900

    Update

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -5,6 +5,9 @@ var header = document.querySelector(".header")
 sidebar.style.display = "block";
 sidebar.style.width = "300px"
 
+
+window.addEventListener("resize", resizeLayout);
+
 for (i = 0; i < navs.length; i++) {
     if (navs[i].classList.contains("subnav--hide") == false) {
         navs[i].previousElementSibling.children[2].classList.toggle("fa-caret-square-down");
@@ -22,6 +25,13 @@ function toggle_subnav(i) {
     i.classList.toggle("fa-caret-square-up");
 }
 
+function resizeLayout() {
+    if (sidebar.style.display == "block")
+        header.style.width = page.style.width = ((document.body.clientWidth || document.documentElement.clientWidth) - parseInt(sidebar.style.width)) + "px";
+    else
+        header.style.width = page.style.width = "100%";
+}
+
 function toggle_sidebar() {
     if (sidebar.style.display == "block") {
         sidebar.style.display = "none";
@@ -29,6 +39,7 @@ function toggle_sidebar() {
     }
     else {
         sidebar.style.display = "block";
-        header.style.width = page.style.width = (document.body.clientWidth || document.documentElement.clientWidth) - 300 + "px";
+        header.style.width = page.style.width = ((document.body.clientWidth || document.documentElement.clientWidth) - parseInt(sidebar.style.width)) + "px";
+        console.log(((document.body.clientWidth || document.documentElement.clientWidth) - parseInt(sidebar.style.width)) + "px");
     }
 }
