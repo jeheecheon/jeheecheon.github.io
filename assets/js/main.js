59ed597b8c21a64becc10129e63530b33da378cf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-23 08:08:22 +0000

    Deploy jeheecheon/jeheecheon.github.io to github.com/jeheecheon/jeheecheon.github.io.git:master

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ var goToIndexBtn = document.querySelector(".button--goToIndex");
 
 var sidebar = document.querySelector(".sidebar");
 var navs = document.getElementsByClassName("subnav");
+var sidebarHamburger = document.querySelector(".sidebar__hamburger");
 
 var page = document.querySelector(".page");
 
@@ -39,19 +40,85 @@ window.addEventListener("scroll", function () {
   getScrolled = true;
 });
 
-window.addEventListener("resize", function () {
+window.addEventListener("resize", initializeLayout);
+//
+//
+//
+//
+//
+function initializeLayout() {
+  var pageWidth =
+    document.body.clientWidth || document.documentElement.clientWidth;
+
+  if (pageWidth < 768) toggleSidebar(1);
+  else toggleSidebar(2);
+}
+
+function resizeLayout() {
   var pageWidth =
     document.body.clientWidth || document.documentElement.clientWidth;
 
   if (pageWidth < 768) {
     if (sidebar.style.right == "-300px") sidebar.style.width = "300px";
     else sidebar.style.width = pageWidth + "px";
+    sidebarHamburger.style.display = "inline-block";
+
   } else {
     sidebar.style.width = "300px";
+    sidebarHamburger.style.display = "none";
   }
-  renewLayout();
-});
 
+  if (sidebar.style.right == "0px") {
+    header.style.width = page.style.width =
+      pageWidth - parseInt(sidebar.style.width) + "px";
+  } else header.style.width = page.style.width = "100%";
+}
+
+//
+//
+//
+//
+//
+//if control value is 1, sidebar will appear and the value is 2, sidebar will disappear
+function toggleSidebar(control) {
+  if (control) {
+    sidebar.style.right = control == 1 ? "-300px" : "0px";
+  } else {
+    if (sidebar.style.right == "0px") sidebar.style.right = "-300px";
+    //(sidebar.style.right == "-300px")
+    else sidebar.style.right = "0px";
+  }
+  resizeLayout();
+}
+
+function toggleSubnav(i) {
+  if (i.parentNode.nextElementSibling.nodeName == "UL") {
+    i.parentNode.nextElementSibling.classList.toggle("subnav--hide");
+    i.parentNode.children[2].classList.toggle("fa-caret-square-down");
+    i.parentNode.children[2].classList.toggle("fa-caret-square-up");
+  } else i.nextElementSibling.click();
+}
+//
+//
+//
+//
+//
+function toggleHeader() {
+  var nowScrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+
+  if (Math.abs(lastScrollTop - nowScrollTop) <= delta) return;
+
+  //when scrolling down
+  if (nowScrollTop > lastScrollTop) {
+    headerBar.parentElement.classList.add("header--hide");
+  }
+  //when scrolling up
+  else {
+    headerBar.parentElement.classList.remove("header--hide");
+  }
+  lastScrollTop = nowScrollTop;
+}
 //
 //
 //
@@ -65,75 +132,4 @@ function includeJs(jsFilePath) {
   js.src = jsFilePath;
 
   document.body.appendChild(js);
-}
-
-//
-//
-//
-//
-//
-function renewLayout() {
-  if (
-    (document.body.clientWidth || document.documentElement.clientWidth) < 768
-  ) {
-    toggleSidebar(1);
-  } else {
-    toggleSidebar(2);
-  }
-}
-
-function resizeLayout() {
-  var pageWidth =
-    document.body.clientWidth || document.documentElement.clientWidth;
-
-  if (sidebar.style.right == "0px") {
-    header.style.width = page.style.width =
-      pageWidth - parseInt(sidebar.style.width) + "px";
-  } else header.style.width = page.style.width = "100%";
-}
-
-//
-//
-//
-//
-//
-function toggleSubnav(i) {
-  if (i.parentNode.nextElementSibling.nodeName == "UL") {
-    i.parentNode.nextElementSibling.classList.toggle("subnav--hide");
-    i.parentNode.children[2].classList.toggle("fa-caret-square-down");
-    i.parentNode.children[2].classList.toggle("fa-caret-square-up");
-  } else i.nextElementSibling.click();
-}
-
-//if control value is 1, sidebar will appear and the value is 2, sidebar will disappear
-function toggleSidebar(control) {
-  if (control) {
-    sidebar.style.right = control == 1 ? "-300px" : "0px";
-  } else {
-    if (sidebar.style.right == "0px") sidebar.style.right = "-300px";
-    //(sidebar.style.right == "-300px")
-    else sidebar.style.right = "0px";
-  }
-  resizeLayout();
-}
-
-//
-//
-//
-//
-//
-function toggleHeader() {
-  var nowScrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-  
-  if (Math.abs(lastScrollTop - nowScrollTop) <= delta) return;
-
-  //when scrolling down
-  if (nowScrollTop > lastScrollTop) {
-    headerBar.parentElement.classList.add("header--hide");
-  }
-  //when scrolling up
-  else {
-    headerBar.parentElement.classList.remove("header--hide");
-  }
-  lastScrollTop = nowScrollTop;
 }
