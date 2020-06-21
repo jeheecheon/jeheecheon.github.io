6339197ec69396249426d92feecb8ff59305879f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-21 15:26:00 +0000

    Deploy jeheecheon/jeheecheon.github.io to github.com/jeheecheon/jeheecheon.github.io.git:master

--- assets/tipuesearch/tipuesearch_content.js
+++
@@ -1,6 +0,0 @@
-var tipuesearch = {"pages": [{
-    "title": "Asd",
-    "text": "블로트 테스트 중 content",
-    "tags": "testing post blog jehee cheon programming web jekyll",
-    "url": "/programming/web/jekyll/2020/01/01/asd/"
-  }]};
