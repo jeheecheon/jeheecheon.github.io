a1c37e1e6a0aa644fb483ae8773fcf44fd04849c
Jehee Cheon <jeheecheon@gmail.com>
2020-06-28 12:48:02 +0900

    Update

--- assets/js/tipuesearch_content.js
+++ assets/js/tipuesearch_content.js
@@ -77,7 +77,7 @@ var tipuesearch = {"pages": [
     "title": {{ document.title | smartify | strip_html | normalize_whitespace | jsonify }},
     "text": {{ document.content | strip_html | normalize_whitespace | jsonify }},
     "tags": {{ taxonomies | join: " " | normalize_whitespace | jsonify }},
-    "url": {{ document.url | relative_url | jsonify }}
+    "url": {{ document.url | prepend: site.baseurl | prepend: site.url | jsonify }}
   }{%- unless forloop.last -%},{%- endunless -%}
 {%- endfor -%}
 ]};
