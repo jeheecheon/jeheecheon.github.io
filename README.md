dc5a1b79be96cf0f395fa49ec8e8ecac60a50ca5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-29 11:03:08 +0000

    Deploy jeheecheon/jeheecheon.github.io to github.com/jeheecheon/jeheecheon.github.io.git:master

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-군대에서 짬날 때 만들고 있는 내 블로그
+군대 싸지방에서 만든 블로그
 
 Designed by Jehee Cheon.\
 Copyright 2020. Jehee Cheon. All rights reserved.
--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -1,1 +1,6 @@
-var tipuesearch = {"pages": []};
+var tipuesearch = {"pages": [{
+    "title": "jekyll로 개인 블로그 개설!",
+    "text": "jekyll 블로그 개설! 블로그 이주했습니다! 우연히 jekyll(static site generator) 를 알게됐고 마침 프론트엔드를 막 공부하던 중이었어서 직접 블로그 theme 을 코딩했보게 됐습니다. 근데 이게.. 그냥 네이버, 다음 블로그처럼 클릭 몇 번 하면 똿! 하고 만들어 지던거 생각하면서 가볍게 시작했는데 생각보다 코드도 꽤 길어지고 쉽지 않더라구요.. 예를 들자면.. html에서 태그 네이밍, 구성 css에서는 float, 익스플로러 호환.. , 메뉴바, 사이드 바(프로필 바, 카테고리 바) js 는 event listenr, 윈도우 크기 변경 시 메뉴바 크기 재조정.., 메뉴바 클릭 구현 등등 뭐,,,, 버그도 많았고 삽질도 몇 번 해보게 됐지만.. 그래도 이렇게 직접 만들어보면서 html, js, css 틀이 전반적으로 잡혔네요. 그리고 군대 사지방에서 공부하면서 만들게 된거라서 개인적으로 좀 뿌듯하네요 ㅎㅎ 이주 전 네이버 블로그",
+    "tags": "jekyll blog 블로그 개설 daily-life",
+    "url": "https://jeheecheon.github.io/daily-life/2020/08/29/my-blog/"
+  }]};
