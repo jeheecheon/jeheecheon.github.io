ded7a9e7ff7c83c6a6e1b1dd0214f704bbe00ed1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-09 13:37:26 +0000

    Deploy jeheecheon/jeheecheon.github.io to github.com/jeheecheon/jeheecheon.github.io.git:master

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -1,4 +1,14 @@
 var tipuesearch = {"pages": [{
+    "title": "Transformations",
+    "text": "vector roation 부분은 거의 기억이 나지가 않는다. 기본적인 2D 회전은 이해하겠지만 3D들어가면 힘들 것 같아서 추가로 이부분 공부하고 넘어가야할 것 같다. learnopengl",
+    "tags": "OpenGL Transformations memo",
+    "url": "https://jeheecheon.github.io/memo/opengl/2021/05/09/Transformations/"
+  },{
+    "title": "Coordinate systems",
+    "text": "좌표개념은 익숙하지 않은 내용이라 여러번 읽어봐야 겠다. learnopengl",
+    "tags": "OpenGL Coordinate Systems memo",
+    "url": "https://jeheecheon.github.io/memo/opengl/2021/05/09/Coordinate-Systems/"
+  },{
     "title": "Textures",
     "text": "이해안되는 내용: NEAREST 와 NEAREST 차이는 대충 어떤지 알겠는데 아래 4가지들 차이점은 잘 모르겠다. 나중에 mipmaps 사용할 기회생기면 직접 모두 출력해보면서 눈으로 확인해봐야 겠다. learnopengl",
     "tags": "OpenGL Textures memo",
